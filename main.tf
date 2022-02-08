ee68135b637200e70ed8de34d681c574337605d4
admin@a3ventures.com <admin@a3ventures.com>
2022-02-08 15:05:50 +0530

    hhh

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "testinstance-01" {
+
+ami = "ami-0cf9783fde88b360f"
+
+instance_type = "t2.small"
+
+subnet_id = "subnet-5611481f"
+
+associate_public_ip_address "{"true"}"
+
+vpc_security_group_ids = [ "sg-00f2016d46494ac9e" ]
+
+key_name {"cloud-collab"}
+
+tags {
+
+Name = "testinstance-01"
+
+}
+
+}
