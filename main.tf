a2d65ebbdac43056da28d9b76841e5357198ef83
nazishalam <32120348+nazishalam@users.noreply.github.com>
2022-02-08 16:04:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-resource "aws_instance" "testinstance-01" {
+resource "aws_instance" "testinstance-02" {
 
-ami = "ami-0cf9783fde88b360f"
+ami = "ami-0332e2eb2b94e44e0"
 
-instance_type = "t2.small"
+instance_type = "t3.large"
 
-subnet_id = "subnet-5611481f"
+subnet_id = "subnet-a2266dca"
 
 associate_public_ip_address "{"true"}"
 
-vpc_security_group_ids = [ "sg-00f2016d46494ac9e" ]
+vpc_security_group_ids = [ "sg-0e091161531fd16b0" ]
 
-key_name {"cloud-collab"}
+key_name {"roombr-staging-admin"}
 
 tags {
 
-Name = "testinstance-01"
+Name = "testinstance-02"
 
 }
 
